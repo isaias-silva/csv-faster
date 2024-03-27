253c11097cf4b8853292ad9ef4abc9235f51277e
isaias-silva <zackblackmangold@gmail.com>
2024-03-27 14:30:36 -0300

    feat: :sparkles: home page

---
+++ frontend/src/components/uploadCsvForm.vue
@@ -0,0 +1,83 @@
+<template>
+  <div class="container">
+    <label for="file"> {{ file?.name||'selecionar arquivo' }} </label>
+    <input
+      id="file"
+      type="file"
+      @change="handleFileUpload"
+      accept=".csv"
+      class="file-input"
+    />
+    <button @click="uploadFile" class="upload-button">Upload</button>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  data() {
+    return {
+      file: null as File | null,
+    };
+  },
+  methods: {
+    handleFileUpload(event: Event) {
+      const target = event.target as HTMLInputElement;
+      if (target.files && target.files[0]) {
+        this.file = target.files[0];
+      }
+    },
+    async uploadFile() {
+      if (!this.file) {
+        alert("Selecione um arquivo CSV primeiro.");
+        return;
+      }
+    },
+  },
+});
+</script>
+
+<style scoped>
+.container {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-items: center;
+}
+
+.container label {
+  margin-top: 20px;
+  margin-bottom: 20px;
+ background-color: rgb(0, 82, 206);
+  padding: 10px 20px;
+  border-radius: 10px;
+  transition: all 0.3s ease;
+  cursor: pointer;
+  color: white;
+}
+
+.container label:hover {
+  background-color: rgb(0, 22, 59);
+
+}
+
+.container input {
+  display: none;
+}
+
+.container button {
+  margin-top: 10px;
+  padding: 10px 20px;
+  background-color: #4CAF50;
+  color: white;
+  border: none;
+  border-radius: 5px;
+  cursor: pointer;
+  transition: background-color 0.3s ease;
+}
+
+.container button:hover {
+  background-color: #45a049;
+}
+</style>
--- frontend/src/views/homePage.vue
+++ frontend/src/views/homePage.vue
@@ -1,9 +1,90 @@
 <template>
-<h1>hohohoh</h1>
+  <div class="container">
+    <h1>CSV-faster</h1>
+    <div class="block-flex">
+      <p>
+        O CSV-Faster é uma ferramenta poderosa projetada para simplificar a
+        análise de dados a partir de arquivos CSV, oferecendo recursos robustos
+        para gerar gráficos informativos de MMR (Monthly Recurring Revenue) e
+        Churn Rate.
+      </p>
+      <p>
+        O arquivo csv para ser tratado corretamente pela aplicação precisa estar
+        de acordo com o exemplo:
+      </p>
+
+      <img src="../assets/image.png" />
+    </div>
+    <UploadCsvForm />
+    <footer>
+      <p>
+        Desenvolvido por
+        <a href="https://github.com/isaias-silva" target="_blank"
+          >Isaias Silva</a
+        >
+      </p>
+    </footer>
+  </div>
 </template>
+
 <script lang="ts">
-import { defineComponent } from 'vue'
+import UploadCsvForm from "@/components/uploadCsvForm.vue";
+import { defineComponent } from "vue";
 export default defineComponent({
-  name: 'HomePage'
-})
+  name: "HomePage",
+  components: { UploadCsvForm },
+});
 </script>
+
+<style scoped>
+.container {
+  max-width: 800px;
+  margin: 0 auto;
+  padding: 20px;
+}
+
+.block-flex {
+  display: flex;
+  width: 100%;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+}
+
+.block-flex p {
+  flex-shrink: 0;
+  width: 100%;
+}
+
+h1 {
+  font-size: 36px;
+  color: #333;
+  margin-bottom: 20px;
+  text-shadow: 1px 2px 10px #4caf50;
+}
+
+p {
+  font-size: 18px;
+  color: #666;
+  line-height: 1.5;
+}
+
+footer {
+  margin-top: 40px;
+  text-align: center;
+}
+
+footer p {
+  font-size: 14px;
+  color: #999;
+}
+
+footer a {
+  color: #007bff;
+  text-decoration: none;
+}
+
+footer a:hover {
+  text-decoration: underline;
+}
+</style>
